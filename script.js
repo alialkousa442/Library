511b8ec46425093624380c975e08e7f77b249e6b
ali alkousa <277460971+alialkousa442@users.noreply.github.com>
2026-09-13 11:12:27 -0700

    Complete core Library app logic with dynamic UI, array CRUD operations, and persistent data storage

---
+++ script.js
@@ -0,0 +1,130 @@
+const addNewBook = document.querySelector("#addBook");
+const Form = document.querySelector("#theForm");
+const Cancel = document.querySelector("#cancel");
+const form = document.querySelector("#form");
+const title = document.querySelector("#title")
+const author = document.querySelector("#author")
+const number = document.querySelector("#nump")
+const check = document.querySelector("#check")
+const container = document.querySelector("#lib-container");
+function Book(title, author, pages, isRead) {
+    if (!new.target)
+        throw new Error("You must use the 'new' keyword");
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author
+    this.pages = pages;
+    this.isRead = isRead;
+}
+const myLibrary = []
+function addBook(title, author, pages, isRead) {
+    const newBook = new Book(title, author, pages, isRead);
+    myLibrary.push(newBook);
+}
+Book.prototype.changeStatus = function () {
+    this.isRead = !this.isRead
+}
+
+
+
+
+
+function saveLocal() {
+    localStorage.setItem("mylibrary", JSON.stringify(myLibrary));
+}
+function loadLocal() {
+    let library = localStorage.getItem("mylibrary");
+    if (library != null) {
+        let books = JSON.parse(library);
+        
+                  myLibrary.length = 0;
+        books.forEach(function (b) {
+            addBook(b.title, b.author, b.pages, b.isRead)
+        });
+        
+    
+    }
+    else {
+        addBook("الليالي البيضاء", "دويستفيسكي", 150, true);
+        addBook("1984", "George Orwell", 328, false);
+        saveLocal()
+    }
+}
+function openModal() {
+    Form.showModal();
+}
+addNewBook.addEventListener("click", openModal)
+function closeModal() {
+    Form.close();
+}
+Cancel.addEventListener("click", closeModal)
+
+function readForm(e) {
+    e.preventDefault();
+    const nTitle = title.value;
+    const nAuthor = author.value;
+    const nNumber = number.value;
+    const nCheck = check.checked;
+    addBook(nTitle, nAuthor, nNumber, nCheck);
+    saveLocal();
+    form.reset();
+    Form.close();
+    displayBooks();
+}
+form.addEventListener("submit", readForm);
+
+
+
+
+function displayBooks() {
+    container.textContent = "";
+    myLibrary.forEach(function (book, index) {
+        const card = document.createElement("div");
+        card.classList.add("stylecard");
+        const title = document.createElement("h3");
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
+        const status = document.createElement("button");
+        const delet = document.createElement("button");
+        delet.textContent = "Delete";
+        status.textContent = book.isRead ? "Read" : "Not Read";
+        function fStatus() {
+            book.changeStatus();
+            saveLocal();
+            displayBooks();
+        }
+        function deleteBook() {
+            myLibrary.splice(index, 1);
+            saveLocal();
+            displayBooks();
+        }
+        delet.addEventListener("click", deleteBook);
+        status.addEventListener("click", fStatus)
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages;
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(status);
+        card.appendChild(delet);
+        container.appendChild(card);
+
+    })
+}
+
+
+
+
+
+
+
+
+loadLocal()
+displayBooks();
+
+
+
+
+
+
